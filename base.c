708579b0d320de7034817249c3ad720d9e39cf9c
Guilherme Sousa Barbosa <guilhermebarbosa.gb2495@gmail.com>
2024-10-22 22:42:38 -0300

    Add files via upload

---
+++ base.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+    FILE *ptrArq;
+    ptrArq = fopen("dtaleat1kdup0.txt","rb");
+    int vet[10000];
+    int size = 0;
+    int value = 0;
+    while(fscanf(ptrArq, "%i", &value) == 1){
+        vet[size++] = value;
+        printf("aa ");
+    }
+    printf(vet);
+}
+
+void insert(char *item, int count){
+   register int a, b; 
+   char t;
+   for(a=1; a<count; a++){
+      t = item[a];
+      for(b=a-1; b>=0 && t<item[b]; b--) 
+         item[b+1] = item[b];
+      item[b+1] = t;
+      }
+}
